cbaae6a04a94d6dab8b3d6ef75723a27d7045f3b
rezahh107 <84147076+rezahh107@users.noreply.github.com>
2026-07-04 21:53:36 +0330

    presentation: add status mapping package

---
+++ src/ev4_transition/presentation/__init__.py
@@ -0,0 +1,8 @@
+from .status_mapping import StatusPresentation, exit_code_for_status, normalize_status, presentation_for_status
+
+__all__ = [
+    "StatusPresentation",
+    "exit_code_for_status",
+    "normalize_status",
+    "presentation_for_status",
+]
