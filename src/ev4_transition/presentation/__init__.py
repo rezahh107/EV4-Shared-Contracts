30fba8a1cb524e68c28899e734c06ba381dba1d3
rezahh107 <84147076+rezahh107@users.noreply.github.com>
2026-07-05 22:12:43 +0330

    feat: export report presentation helpers

--- src/ev4_transition/presentation/__init__.py
+++ src/ev4_transition/presentation/__init__.py
@@ -1,8 +1,16 @@
+from .bidi import html_code_ltr, isolate_ltr_text, markdown_code_ltr
 from .status_mapping import StatusPresentation, exit_code_for_status, normalize_status, presentation_for_status
+from .theme_tokens import THEME_TOKENS, assert_theme_contract, theme_tokens
 
 __all__ = [
     "StatusPresentation",
+    "THEME_TOKENS",
+    "assert_theme_contract",
     "exit_code_for_status",
+    "html_code_ltr",
+    "isolate_ltr_text",
+    "markdown_code_ltr",
     "normalize_status",
     "presentation_for_status",
+    "theme_tokens",
 ]
